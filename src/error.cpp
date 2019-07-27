f9806470421e701be50992c582b808881ae948fc
Mikhail Zinin <mzinin@gmail.com>
2019-07-27 17:44:17 +0200

    Implement Error and Token classes

---
+++ src/error.cpp
@@ -0,0 +1,12 @@
+#include <error.hpp>
+
+
+s2e2::Error::Error(const std::string& message)
+    : std::runtime_error(message)
+{
+}
+
+s2e2::Error::Error(const char* message)
+    : std::runtime_error(message)
+{
+}
