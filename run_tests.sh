173be0b3123bded91bd5f107387949e85394e79f
Mikhail Zinin <mzinin@gmail.com>
2019-08-01 20:58:22 +0200

    Run unit tests in random order

--- run_tests.sh
+++ run_tests.sh
@@ -24,4 +24,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 BUILD_SUB_DIR=$(echo "$BUILD_TYPE" | awk '{print tolower($0)}')
 TESTS_DIR="${SCRIPT_DIR}/build/output/${BUILD_SUB_DIR}/test"
 
-"${TESTS_DIR}/s2e2_tests"
+"${TESTS_DIR}/s2e2_tests" --gtest_shuffle
