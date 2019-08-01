6b1680d6a10edfffe7f9c9853d475dc2a597615d
Mikhail Zinin <mzinin@gmail.com>
2019-08-01 21:50:06 +0200

    Add unit tests for FunctionAddDays

--- test/src/test_utils.hpp
+++ test/src/test_utils.hpp
@@ -7,9 +7,9 @@
 namespace TestUtils
 {
     template <class ... Args>
-    std::stack<std::any> createStack(Args ... args)
+    std::stack<std::any> createStack(Args&& ... args)
     {
-        return std::stack<std::any>{std::stack<std::any>::container_type{args...}};
+        return std::stack<std::any>{std::stack<std::any>::container_type{std::move(args)...}};
     }
 
 } // namespace TestUtils
