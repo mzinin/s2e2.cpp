da54d04836b5ccb5b8489693d4545e1ebf3a13f3
Mikhail Zinin <mzinin@gmail.com>
2019-08-03 21:37:12 +0200

    Implement OperatorNot class

---
+++ include/operators/operator_not.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <operator.hpp>
+
+#include <any>
+
+
+namespace s2e2
+{
+    /**
+     * @class OperatorNot
+     * @brief Operator !
+     * @details Negates boolean value.
+     */
+    class OperatorNot final : public Operator
+    {
+    public:
+        /**
+         * Default constructor.
+         */
+        OperatorNot();
+
+    private:
+        /**
+         * @brief Check if arguments are correct.
+         * @returns true is arguments are correct, false otherwise.
+         */
+        bool checkArguments() const override;
+
+        /**
+         * @brief Calculate result of the function.
+         * @return Result.
+         */
+        std::any result() const override;
+    };
+
+} // namespace s2e2
