d107700923d8c5de1e33ba10c0a7b84c52e6a298
Mikhail Zinin <mzinin@gmail.com>
2019-08-04 15:52:25 +0200

    Implement OperatorPlus class

---
+++ include/operators/operator_plus.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <operator.hpp>
+
+#include <any>
+
+
+namespace s2e2
+{
+    /**
+     * @class OperatorPlus
+     * @brief Operator +
+     * @details Concatenates two strings.
+     */
+    class OperatorPlus final : public Operator
+    {
+    public:
+        /**
+         * Default constructor.
+         */
+        OperatorPlus();
+
+    private:
+        /**
+         * @brief Check if arguments are correct.
+         * @returns true is arguments are correct, false otherwise.
+         */
+        bool checkArguments() const override;
+
+        /**
+         * @brief Calculate result of the function.
+         * @return Result.
+         */
+        std::any result() const override;
+    };
+
+} // namespace s2e2
